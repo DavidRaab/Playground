6009fa3da5887f31efcce7a5b5d14a72b4eb47a2
David Raab <davidraab83@gmail.com>
2022-09-28 17:39:34 +0200

    Re-written add
    I thought that maybe it would be faster with less garbage collection.
    But in overall it has no measured impact so-far. Maybe testing in
    a full compiled environment makes more sense.

--- tree.fsx
+++ tree.fsx
@@ -54,6 +54,94 @@ module Tree =
             singleton value
         | Node2(Empty,x,Empty) ->
             if value <= x then n3 value x else n3 x value
+        | Node2(a,x,b) ->
+            if value <= x then
+                match a with
+                | Node4(Empty,y1,Empty,y2,Empty,y3,Empty) ->
+                    if   value < y1 then (node3 (n3 value y1)  y2 (singleton y3) x b)
+                    elif value < y2 then (node3 (n3 y1 value)  y2 (singleton y3) x b)
+                    elif value < y3 then (node3 (singleton y1) y2 (n3 value y3)  x b)
+                    else                 (node3 (singleton y1) y2 (n3 y3 value)  x b)
+                | Node4(c1,y1,c2,y2,c3,y3,c4) ->
+                    if   value < y1 then (node3 (node2 (add value c1) y1 c2) y2 (node2 c3 y3 c4) x b)
+                    elif value < y2 then (node3 (node2 c1 y1 (add value c2)) y2 (node2 c3 y3 c4) x b)
+                    elif value < y3 then (node3 (node2 c1 y1 c2)             y2 (node2 (add value c3) y3 c4) x b)
+                    else                 (node3 (node2 c1 y1 c2)             y2 (node2 c3 y3 (add value c4)) x b)
+                | a -> node2 (add value a) x b
+            else
+                match b with
+                | Node4(Empty,y1,Empty,y2,Empty,y3,Empty) ->
+                    if   value < y1 then (node3 a x (n3 value y1)  y2 (singleton y3))
+                    elif value < y2 then (node3 a x (n3 y1 value)  y2 (singleton y3))
+                    elif value < y3 then (node3 a x (singleton y1) y2 (n3 value y3))
+                    else                 (node3 a x (singleton y1) y2 (n3 y3 value))
+                | Node4(c1,y1,c2,y2,c3,y3,c4) ->
+                    if   value < y1 then (node3 a x (node2 (add value c1) y1 c2) y2 (node2 c3 y3 c4))
+                    elif value < y2 then (node3 a x (node2 c1 y1 (add value c2)) y2 (node2 c3 y3 c4))
+                    elif value < y3 then (node3 a x (node2 c1 y1 c2)             y2 (node2 (add value c3) y3 c4))
+                    else                 (node3 a x (node2 c1 y1 c2)             y2 (node2 c3 y3 (add value c4)))
+                | b -> node2 a x (add value b)
+        | Node3(Empty,x,Empty,y,Empty) ->
+            if   value <= x then n4 value x y
+            elif value <= y then n4 x value y
+                            else n4 x y value
+        | Node3(a,x,b,y,c) ->
+            if value <= x then
+                match a with
+                | Node4(Empty,y1,Empty,y2,Empty,y3,Empty) ->
+                    if   value <= y1 then (node4 (n3 value y1)  y2 (singleton y3) x b y c)
+                    elif value <= y2 then (node4 (n3 y1 value)  y2 (singleton y3) x b y c)
+                    elif value <= y3 then (node4 (singleton y1) y2 (n3 value y3)  x b y c)
+                    else                  (node4 (singleton y1) y2 (n3 y3 value)  x b y c)
+                | Node4(c1,y1,c2,y2,c3,y3,c4) ->
+                    if   value <= y1 then (node4 (node2 (add value c1) y1 c2) y2 (node2 c3 y3 c4) x b y c)
+                    elif value <= y2 then (node4 (node2 c1 y1 (add value c2)) y2 (node2 c3 y3 c4) x b y c)
+                    elif value <= y3 then (node4 (node2 c1 y1 c2)             y2 (node2 (add value c3) y3 c4) x b y c)
+                    else                  (node4 (node2 c1 y1 c2)             y2 (node2 c3 y3 (add value c4)) x b y c)
+                | a -> node3 (add value a) x b y c
+            elif value <= y then
+                match b with
+                | Node4(Empty,y1,Empty,y2,Empty,y3,Empty) ->
+                    if   value <= y1 then (node4 a x (n3 value y1)  y2 (singleton y3) y c)
+                    elif value <= y2 then (node4 a x (n3 y1 value)  y2 (singleton y3) y c)
+                    elif value <= y3 then (node4 a x (singleton y1) y2 (n3 value y3)  y c)
+                    else                  (node4 a x (singleton y1) y2 (n3 y3 value)  y c)
+                | Node4(c1,y1,c2,y2,c3,y3,c4) ->
+                    if   value <= y1 then (node4 a x (node2 (add value c1) y1 c2) y2 (node2 c3 y3 c4) y c)
+                    elif value <= y2 then (node4 a x (node2 c1 y1 (add value c2)) y2 (node2 c3 y3 c4) y c)
+                    elif value <= y3 then (node4 a x (node2 c1 y1 c2)             y2 (node2 (add value c3) y3 c4) y c)
+                    else                  (node4 a x (node2 c1 y1 c2)             y2 (node2 c3 y3 (add value c4)) y c)
+                | b -> node3 a x (add value b) y c
+            else
+                match c with
+                | Node4(Empty,y1,Empty,y2,Empty,y3,Empty) ->
+                    if   value <= y1 then (node4 a x b y (n3 value y1)  y2 (singleton y3))
+                    elif value <= y2 then (node4 a x b y (n3 y1 value)  y2 (singleton y3))
+                    elif value <= y3 then (node4 a x b y (singleton y1) y2 (n3 value y3))
+                    else                  (node4 a x b y (singleton y1) y2 (n3 y3 value))
+                | Node4(c1,y1,c2,y2,c3,y3,c4) ->
+                    if   value <= y1 then (node4 a x b y (node2 (add value c1) y1 c2) y2 (node2 c3 y3 c4))
+                    elif value <= y2 then (node4 a x b y (node2 c1 y1 (add value c2)) y2 (node2 c3 y3 c4))
+                    elif value <= y3 then (node4 a x b y (node2 c1 y1 c2)             y2 (node2 (add value c3) y3 c4))
+                    else                  (node4 a x b y (node2 c1 y1 c2)             y2 (node2 c3 y3 (add value c4)))
+                | c -> node3 a x b y (add value c)
+        | Node4(Empty,x,Empty,y,Empty,z,Empty) ->
+            if   value <= x then (node2 (n3 value x)  y (singleton z))
+            elif value <= y then (node2 (n3 x value)  y (singleton z))
+            elif value <= z then (node2 (singleton x) y (n3 value z))
+            else                 (node2 (singleton x) y (n3 z value))
+        | Node4(a,x,b,y,c,z,d) ->
+            if   value <= x then (node2 (node2 (add value a) x b) y (node2 c z d))
+            elif value <= y then (node2 (node2 a x (add value b)) y (node2 c z d))
+            elif value <= z then (node2 (node2 a x b) y (node2 (add value c) z d))
+            else                 (node2 (node2 a x b) y (node2 c z (add value d)))
+
+    let add' value tree =
+        match tree with
+        | Empty ->
+            singleton value
+        | Node2(Empty,x,Empty) ->
+            if value <= x then n3 value x else n3 x value
         | Node2(IsNode4 a,x,b) when value <= x ->
             match add value a with
             | Node2(c1,y,c2) -> node3 c1 y c2 x b
@@ -114,13 +202,13 @@ module Tree =
         | Node3(l,x,m,y,r)       -> List.concat [toList l; [x]; toList m;  [y]; toList r]
         | Node4(l,x,c1,y,c2,z,r) -> List.concat [toList l; [x]; toList c1; [y]; toList c2; [z]; toList r]
 
-let numbers = shuffle [|1..32|]
+let numbers = shuffle [|1..1_000_000|]
 // printfn "Numbers: %A" numbers
 
-printfn "Create Tree ..."
-let treeA = timeIt (fun () -> Array.fold (fun t x -> Tree.add x    t) Tree.empty numbers)
-// printfn "Create Map ..."
-// let treeB = timeIt (fun () -> Array.fold (fun m x -> Map.add  x () m) Map.empty  numbers)
+printfn "Create TreeA ..."
+let treeA = timeIt (fun () -> Array.fold (fun t x -> Tree.add  x t) Tree.empty numbers)
+printfn "Create TreeB ..."
+let treeB = timeIt (fun () -> Array.fold (fun t x -> Tree.add' x t) Tree.empty numbers)
 
 
 let isSorted xs =
@@ -132,11 +220,12 @@ let isSorted xs =
         ) (true,List.head xs) (List.tail xs)
         |> fst
 
-// for treeA in treeA do
-printfn "%s" (Tree.show treeA)
-printfn "Tree List: %A" (Tree.toList treeA)
-printfn "IsSorted %b" (isSorted (Tree.toList treeA))
-printfn "DepthA %d" (Tree.depth treeA)
-printfn ""
+for tree in [treeA;treeB] do
+    // for treeA in treeA do
+    // printfn "%s" (Tree.show treeA)
+    printfn "Tree List: %A" (Tree.toList tree)
+    printfn "IsSorted %b" (isSorted (Tree.toList tree))
+    printfn "DepthA %d" (Tree.depth tree)
+    printfn ""
 
 
