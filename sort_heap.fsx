4aa871e6f480ff273ed54e1f07fb728c199e4957
David Raab <davidraab83@gmail.com>
2022-10-04 01:26:25 +0200

    Heap Sort
    Changed Heap to order from smallest to biggest. Used this
    to implement Heap Sort.

--- sort_heap.fsx
+++ sort_heap.fsx
@@ -1,5 +1,8 @@
 #!/usr/bin/env -S dotnet fsi
 
+#load "FSExtensions.fsx"
+open FSExtensions
+
 type Heap<'a when 'a : comparison>(?depth) =
     let mutable depth    = defaultArg depth 4
     let mutable maxCount = int (2.0 ** float depth) - 1
@@ -10,8 +13,8 @@ type Heap<'a when 'a : comparison>(?depth) =
     let left   x = 2 * x
     let right  x = 2 * x + 1
 
-    member _.Count     = count
-    member _.Allocated = heap.Length - 1
+    member _.Count    = count
+    member _.Capacity = heap.Length - 1
 
     override _.ToString () =
         let rec show (d:int) =
@@ -40,7 +43,7 @@ type Heap<'a when 'a : comparison>(?depth) =
             if idx = 1 then () else
                 let c = heap.[idx]
                 let p = heap.[parent idx]
-                if c > p then
+                if c < p then
                     heap.[idx]        <- p
                     heap.[parent idx] <- c
                     loop (parent idx)
@@ -53,7 +56,7 @@ type Heap<'a when 'a : comparison>(?depth) =
     member _.Peek () =
         heap.[1]
 
-    member _.Remove () =
+    member _.RemoveMin () =
         if count = 0 then
             ValueNone
         else
@@ -72,34 +75,42 @@ type Heap<'a when 'a : comparison>(?depth) =
                     let l = heap.[left idx]
                     let r = heap.[right idx]
 
-                    if l > r && l > current then
+                    if l < r && l < current then
                         heap.[idx]      <- l
                         heap.[left idx] <- current
                         loop (left idx)
-                    elif r > current then
+                    elif r < current then
                         heap.[idx]       <- r
                         heap.[right idx] <- current
                         loop (right idx)
                 // if index only has left child
                 elif idx*2 <= count then
                     let l = heap.[left idx]
-                    if l > current then
+                    if l < current then
                         heap.[idx]      <- l
                         heap.[left idx] <- current
                         loop (left idx)
             loop 1
             max
 
+// Generate a shuffled Array
+let array = Array.shuffle [|1..100|]
+printfn "Array: %A" array
 
-
+// Add shuffled Array to Heap
 let heap = Heap<int>()
-heap.AddMany [10]
-heap.AddMany [20;5;3]
-heap.AddMany [12;16;9;100]
-heap.AddMany [33;42;11;4;1;88;66]
-printfn "%O" heap
+heap.AddMany array
+printfn "Heap:\n%O\n" heap
 
-for i=0 to 20 do
-    printfn "%O" heap
-    printfn "Max: %A" (heap.Remove ())
+// Remove Minimum until heap is empty and add it to a ResizeArray
+let sorted = ResizeArray()
+let rec loop () =
+    heap.RemoveMin() |> ValueOption.iter (fun x ->
+        sorted.Add x
+        loop ()
+    )
+loop ()
+
+// Print Sorted Array
+printfn "%A" (sorted.ToArray())
 
