650e8d5dd10e3a56fcd58c7f1de7dc9bbc287571
David Raab <davidraab83@gmail.com>
2022-10-06 04:13:16 +0200

    Some Text improvements

--- Articles/parenthesis.fsx
+++ Articles/parenthesis.fsx
@@ -14,12 +14,12 @@ you get it.
 state.Add (Rect.create (Vector2(float32 x * 11f, float32 y * 11f + yOffset)) Color.White)
 
 (*
-so, not every call is in this tuple syntax. But the call to create the `Vector2` is.
+Not every function call uses tuple syntax. But the call to create the `Vector2` is.
 I am used to read this kind of inlining, and I think it becomes natural in a functional
 language that returns values instead of modifing them.
 
-But I still have problem reading those line, becaue the parenthesis, don't match up
-nicely. Just let me extract the `Vector2` first. I write:
+But I still have a problem reading the above line and it has all to do becuase Vector2
+uses Tuple syntax. Let me show you what i mean: First we extract the `Vector2`.
 
     Vector2(float32 x * 11f, float32 y * 11f + yOffset)
 
@@ -30,7 +30,7 @@ Why is this line so hard to read?
 
 And I think a comma is a very low visual clue that can be easily overseen. And
 still after all of that I have to put parenthesis around `(Vector2 ...)` because
-if I don't do that the language will see the code like this.
+when I don't do that the language will see the code like this.
 
     Function    1.Argument 2.Argument     3.Argument
     Rect.create Vector2    (float32 ....) Color.White
@@ -42,12 +42,12 @@ and just separated by empty spaces. Now the `Vector2` line can be written as fol
 
     Vector2 (float32 x * 11f) (float32 y * 11f + yOffSet)
 
-This is still considered LISP-like. Even if the sorounding parenthesis are omitted. But
+This is still considered LISP-like. Even if the surrounding parenthesis are omitted. But
 ML-Like languages (ML, F#, Haskell, ...) are allowed to omit these parenthesis if possible.
 
-But still you see them arounf `(float32 x * 11f)`. And in my opinion it is much easier to see
+But still you see them around `(float32 x * 11f)`. And in my opinion it is much easier to see
 that `Vector2` is called with 2 arguments. I can cleary and visualy separate those
-arguments, and understand that those are computed by another expression.
+arguments and understand that those are computed by another expression.
 
 When I insert the `Vector2` line again i now end up with.
 
@@ -56,7 +56,7 @@ When I insert the `Vector2` line again i now end up with.
 And in my opinion its easier to read. Here is a more simplified example to see the difference.
 
     state.Add (Rect.create (Vector2(x,y)) Color.White)
-    
+
 vs.
 
     state.Add (Rect.create (Vector2.create x y) Color.White)
@@ -65,7 +65,7 @@ and still consider that `state.Add` and `Rect.create` are already ML/LISP-like, 
 the first line would be written.
 
     state.Add(Rect.create(Vector2(x, y), Color.White))
-    
+
 The amount of parenthesis are the same as a full LISP-like syntax. The only difference is
 the position. It is `(func x y)` instead of `func(x,y)` but the first version is
 much easier to read. Especially in code where every argument can become another more
