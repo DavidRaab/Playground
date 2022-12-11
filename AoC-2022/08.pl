2c8eb91203c4051501f2f063933a7905ac98a655
David Raab <davidraab83@gmail.com>
2022-12-11 12:45:41 +0100

    Changed Solution
    Instead of the foreach loop i create functions that returns the
    top, left, right and bottom element from a given position.

--- AoC-2022/08.pl
+++ AoC-2022/08.pl
@@ -5,16 +5,14 @@ use feature 'signatures';
 no warnings 'experimental::signatures';
 use open ':std', ':encoding(UTF-8)';
 use Data::Printer;
-use List::Util qw(max sum0);
+use List::Util qw(max sum0 all);
 
 # read file as Array of Array
 my @forest = map {[grep { not m/\A\s+\z/ } split //]} <>;
 my $width  = max map { scalar @$_ } @forest;
 my $height = @forest;
 
-printf "W: %d H: %d\n", $width, $height;
-
-print_aoa(\@forest);
+# print_aoa(\@forest);
 
 my @seen;
 for (my $y=0; $y < $height; $y++) {
@@ -25,76 +23,39 @@ for (my $y=0; $y < $height; $y++) {
     push @seen, \@result;
 }
 
-print_aoa(\@seen);
+# print_aoa(\@seen);
 
 # Add Visible trees together
 my $visible = sum0 map {map { s/\A([TLBR])\z/1/gr } @$_} @seen;
 printf "Visible: %d\n", $visible;
 
+sub range ($x,$y) {
+    return $x <= $y ? $x .. $y : reverse $y .. $x;
+}
+
+sub top_trees ($x, $y, $forest) {
+    return $y == 0         ? () : map { $forest->[$_][$x] } range($y-1, 0);
+}
+
+sub bottom_trees ($x, $y, $forest) {
+    return $y == $height-1 ? () : map { $forest->[$_][$x] } range($y+1,$height-1);
+}
+
+sub left_trees ($x, $y, $forest) {
+    return $x == 0         ? () : map { $forest->[$y][$_] } range($x-1,0);
+}
+
+sub right_trees ($x, $y, $forest) {
+    return $x == $width-1  ? () : map { $forest->[$y][$_] } range($x+1,$width-1);
+}
+
 
 sub can_be_seen ($x, $y, $forest) {
     my $current = $forest->[$y][$x];
-
-    # Scan to left
-    my $left_visible = 1;
-    if ( $x == 0 ) {
-        return 1;
-    }
-    else {
-        for (my $i=$x-1; $i >= 0; $i--) {
-            if ( $forest->[$y][$i] >= $current ) {
-                $left_visible = 0;
-                last;
-            }
-        }
-    }
-    return "L" if $left_visible;
-
-    # Scan to Right
-    my $right_visible = 1;
-    if ( $x == $width-1 ) {
-        return 1;
-    }
-    else {
-        for (my $i=$x+1; $i < $width; $i++) {
-            if ( $forest->[$y][$i] >= $current ) {
-                $right_visible = 0;
-                last;
-            }
-        }
-    }
-    return "R" if $right_visible;
-
-    # Scan to Top
-    my $top_visible = 1;
-    if ( $y == 0 ) {
-        return 1;
-    }
-    else {
-        for (my $i=$y-1; $i >= 0; $i--) {
-            if ( $forest->[$i][$x] >= $current ) {
-                $top_visible = 0;
-                last;
-            }
-        }
-    }
-    return "T" if $top_visible;
-
-    # Scan to Bottom
-    my $bottom_visible = 1;
-    if ( $y == $height-1 ) {
-        return 1;
-    }
-    else {
-        for (my $i=$y+1; $i < $height; $i++) {
-            if ( $forest->[$i][$x] >= $current ) {
-                $bottom_visible = 0;
-                last;
-            }
-        }
-    }
-    return "B" if $bottom_visible;
-
+    return "L" if all { $current > $_ } left_trees  ($x,$y,$forest);
+    return "R" if all { $current > $_ } right_trees ($x,$y,$forest);
+    return "T" if all { $current > $_ } top_trees   ($x,$y,$forest);
+    return "B" if all { $current > $_ } bottom_trees($x,$y,$forest);
     return 0;
 }
 
