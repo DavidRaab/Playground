da66213b771a381e6ad2b31ecea14f720b5c549d
David Raab <davidraab83@gmail.com>
2022-12-18 20:40:45 +0100

    Some Performance improvements
    More than doubles the speed.

--- AoC-2022/12.pl
+++ AoC-2022/12.pl
@@ -53,7 +53,7 @@ printf "Path Length: %d\n", (scalar @$path - 1);
 # Computes the Dijkstra map
 sub dijkstra($input, $start) {
     # Empty initialized target map for dijkstra algorithm
-    my $field = Array2D->init($map->width, $map->height, sub { undef } );
+    my $field = Array2D->init($map->width, $map->height, sub { -1 } );
 
     # Consider Pos(-1,-1) as target
     $field->set($start, Pos->new(-1,-1));
@@ -61,14 +61,15 @@ sub dijkstra($input, $start) {
     # Compute Dijkstra Map
     my @queue = ($start);
     while ( my $pos = shift @queue ) {
+        my ($x, $y) = $pos->xy;
         my $current = $input->get($pos);
 
         for my $next ( $pos->top, $pos->right, $pos->bottom, $pos->left ) {
-            next if not $input->is_inside($next);
             my $next_value = $input->get($next);
+            next if not defined $next_value;
 
-            if ( $next_value <=  $current + 1 ) {
-                if ( not defined $field->get($next) ) {
+            if ( $next_value <= $current + 1 ) {
+                if ( not ref $field->get($next) ) {
                     $field->set($next, $pos);
                     push @queue, $next;
                 }
@@ -102,6 +103,7 @@ no warnings 'experimental::signatures';
 sub new     ($class,$x,$y) { bless [$x,$y], $class }
 sub x       ($self) { $self->[0] }
 sub y       ($self) { $self->[1] }
+sub xy      ($self) { return @$self }
 sub top     ($self) { Pos->new($self->x    , $self->y - 1) }
 sub right   ($self) { Pos->new($self->x + 1, $self->y    ) }
 sub bottom  ($self) { Pos->new($self->x    , $self->y + 1) }
@@ -153,18 +155,24 @@ sub height ($self) { return $self->{height} }
 sub width  ($self) { return $self->{width}  }
 
 sub is_inside ($self, $pos) {
-    my ($w, $h) = ($self->width, $self->height);
-    if ( $pos->x >= 0 && $pos->x < $w ) {
-        if ( $pos->y >= 0 && $pos->y < $h ) {
-            return 1;
-        }
+    my ($x, $y) = $pos->xy;
+    if ( $x >= 0 && $y >= 0 && $x < $self->{width} && $y < $self->{height} ) {
+        return 1;
     }
     return;
 }
 
 sub get($self, $pos) {
-    if ( $self->is_inside($pos) ) {
-        return $self->{data}[$pos->y][$pos->x];
+    my ($x, $y) = $pos->xy;
+    if ( $x >= 0 && $y >= 0 && $x < $self->{width} && $y < $self->{height} ) {
+        return $self->{data}[$y][$x];
+    }
+    return;
+}
+
+sub get_xy($self, $x, $y) {
+    if ( $x >= 0 && $y >= 0 && $x < $self->{width} && $y < $self->{height} ) {
+        return $self->{data}[$y][$x];
     }
     return;
 }
@@ -173,10 +181,14 @@ sub set($self, $pos, $value) {
     $self->{data}[$pos->y][$pos->x] = $value;
 }
 
+sub set_xy($self, $x, $y, $value) {
+    $self->{data}[$y][$x] = $value;
+}
+
 sub reduce ($self, $init, $f) {
     for my $y ( 0 .. $self->height - 1 ) {
         for my $x ( 0 .. $self->width - 1 ) {
-            $init = $f->($init, $self->get(Pos->new($x,$y)), $x, $y);
+            $init = $f->($init, $self->get_xy($x,$y), $x, $y);
         }
     }
     return $init;
@@ -185,7 +197,7 @@ sub reduce ($self, $init, $f) {
 sub iter ($self, $f) {
     for my $y ( 0 .. $self->height - 1 ) {
         for my $x ( 0 .. $self->width - 1 ) {
-            $f->(Pos->new($x,$y), $self->get(Pos->new($x,$y)));
+            $f->(Pos->new($x,$y), $self->get_xy($x,$y));
         }
     }
 }
