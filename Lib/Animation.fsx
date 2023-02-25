d697803d7b2487e168afe932f4d8107bb1d99bdd
David Raab <davidraab83@gmail.com>
2023-02-25 02:37:12 +0100

    Added Animation.toList
    This function makes testing easier and can be part of the Animation.
    Dunno if that is helpful beyond that.

--- Lib/Animation.fsx
+++ Lib/Animation.fsx
@@ -18,30 +18,17 @@ type Anim<'a>      = Anim      of (TimeSpan -> AnimationResult<'a>)
 type Animation<'a> = Animation of (unit     -> Anim<'a>)
 
 module Anim =
-    let run   deltaTime (Anim f) = f deltaTime
+    let run deltaTime (Anim f) =
+        f deltaTime
+
     let value deltaTime anim =
         match run deltaTime anim with
         | Running  v     -> v
         | Finished (v,t) -> v
 
-    // let create f =
-    //     let mutable finished = ValueNone
-    //     Anim(fun dt ->
-    //         match finished with
-    //         | ValueNone
-    //         | ValueSome (Running (_,_)) ->
-    //             finished <- ValueSome (f dt)
-    //             finished
-    //         | ValueSome (Finished (v,t)) ->
-    //             finished
-    //     )
-
 module Animation =
     let wrap x =
         Animation(fun () -> Anim(fun deltaTime -> Finished(x,deltaTime)))
-
-    let empty =
-        wrap ()
 
     /// Runs an animation by returning Anim<'a>
     let run (Animation f) =
@@ -51,16 +38,41 @@ module Animation =
     let create f =
         Animation(fun () -> Anim f)
 
-    /// Transforms a lerping function into an animation
+    /// Transforms a lerping function into an animation. It takes `duration` time
+    /// to run from 0.0 to 1.0. This is passed to the lerping function that must
+    /// return the actual value.
     let fromLerp f (duration:TimeSpan) =
         Animation(fun () ->
             let mutable soFar = TimeSpan.Zero
             Anim(fun dt ->
                 soFar <- soFar + dt
-                if soFar < duration then
-                    let fraction = soFar / duration
-                    Running  (f fraction)
-                else
-                    Finished (f 1.0, duration - soFar)
+                if   soFar < duration
+                then Running  (f (soFar / duration))
+                else Finished (f 1.0, duration - soFar)
             )
         )
+
+    /// Turns a whole animation into a list by simulating it with `stepTime`
+    let toList anim stepTime =
+        let anim = run anim
+        [
+            let mutable running = true
+            while running do
+                match Anim.run stepTime anim with
+                | Running x ->
+                    yield x
+                | Finished (x,_) ->
+                    running <- false
+                    yield x
+        ]
+
+
+module Lerp =
+    let int (start:int) (stop:int) fraction =
+        let start = float start
+        let stop  = float stop
+        int ((start * (1.0 - fraction)) + (stop * fraction))
+
+    let float start stop fraction =
+        ((start * (1.0 - fraction)) + (stop * fraction))
+
--- animation.fsx
+++ animation.fsx
@@ -9,45 +9,38 @@ open Test
 let sec x = TimeSpan.FromSeconds x
 let ms  x = TimeSpan.FromMilliseconds x
 
-let lerp start stop fraction =
-    (start * (1.0 - fraction)) + (stop * fraction)
-
-let is_anim anim dt xs name =
-    let anim      = Animation.run anim
-    let mutable i = 0
-    for x in xs do
-        let value = Anim.value dt anim
-        Test.is value x (sprintf "%s index %d" name i)
-        i <- i + 1
-
-let finishedAfter anim dt value name =
-    match Anim.run dt (Animation.run anim) with
-    | Running   x    -> Test.fail name
-    | Finished (x,t) -> Test.is x value name
-
-let runningAt anim dt value name =
-    match Anim.run dt (Animation.run anim) with
-    | Running   x    -> Test.is x value name
-    | Finished (x,t) -> Test.fail name
-
-// Testing
-is_anim (Animation.wrap 1) (sec 1) [1;1;1] "is 1"
-is_anim (Animation.wrap 5) (sec 1) [5;5;5] "is 5"
-
-finishedAfter (Animation.wrap 1) (sec 0) 1 "wrap(1) is finished"
-finishedAfter (Animation.wrap 5) (sec 0) 5 "wrap(5) is finished"
+// Animation.wrap
+Test.is (Animation.toList (Animation.wrap 1) (sec 1)) [1] "wrap(1) is [1]"
+Test.is (Animation.toList (Animation.wrap 5) (sec 0)) [5] "wrap(5) is [5]"
 
 Test.is
     (Anim.run (sec 1) (Animation.run (Animation.wrap 1)))
     (Finished (1, sec 1))
     "wrap 1 finished with 1 seconds left"
 
-let ten1sec  = Animation.fromLerp (lerp 1 10) (sec  1)
-let ten2sec  = Animation.fromLerp (lerp 1 10) (sec  2)
-let ten10sec = Animation.fromLerp (lerp 1 10) (sec 10)
+// Animation that runs from 1.0 to 10.0 ...
+let toTen = Animation.fromLerp (Lerp.float 1 10)
 
-is_anim ten1sec  (ms 500) [5.5;  10.0; 10.0] "toTen in 1 sec"
-is_anim ten2sec  (ms 500) [3.25;  5.5;  7.75; 10.0; 10.0] "toTen in 2 sec"
-is_anim ten10sec (sec 3)  [3.7;   6.4;  9.1;  10.0; 10.0] "toTen in 10 sec"
+Test.is
+    (Animation.toList (toTen (sec 1)) (ms 500))
+    [5.5; 10]
+    "Lerp.float 1 10 in 1 seconds"
+
+Test.is
+    (Animation.toList (toTen (sec 2)) (ms 500))
+    [3.25;  5.5;  7.75; 10.0]
+    "Lerp.float 1 10 in 2 seconds"
+
+Test.is
+    (Animation.toList (toTen (sec 10)) (ms 500))
+    [1.45; 1.9; 2.35; 2.8; 3.25; 3.7; 4.15; 4.6; 5.05; 5.5; 5.95; 6.4; 6.85; 7.3; 7.75; 8.2; 8.65; 9.1; 9.55; 10.0]
+    "Lerp.float 1 10 in 10 seconds"
+
+// Animation with Lerp.int
+Test.is
+    (Animation.toList (Animation.fromLerp (Lerp.int 0 5) (sec 1)) (ms 100))
+    [0;1;1;2;2;3;3;4;4;5]
+    "Lerp.int 0 5 with 100ms"
+
 
 Test.doneTesting ()
