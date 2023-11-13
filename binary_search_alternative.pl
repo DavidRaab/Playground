e600a866d7401ee88d7934f5b141dc42e128771d
David Raab <davidraab83@gmail.com>
2023-11-13 15:05:56 +0100

    Some cleanups

--- binary_search_alternative.pl
+++ binary_search_alternative.pl
@@ -15,7 +15,7 @@ my ($opt, $usage) = describe_options(
 $usage->die if $opt->help;
 
 
-# Example of Parsing CSV as Data
+# Example of Parsing CSV
 my $csv = Text::CSV->new({
     sep_char        => ',',
     skip_empty_rows => 1,
@@ -39,9 +39,7 @@ my @big_data = map {
     # At every iteration add 1-3 to $current_id. This way i get unique
     # "random" ids
     $current_id += (int rand(3)) + 1;
-    my $id   = $current_id;
-    my $name = "foo";
-    { id => $id, name => $name }
+    { id => $current_id, name => "foo" }
 } 1 .. 100_000;
 
 # generate lookup table
@@ -72,19 +70,17 @@ use Test2::Bundle::More;
     for my $check ( @checks ) {
         my $search   = $check->[0];
         my $expected = $check->[1];
+        my $got      = get_index($lookup, $search);
 
-        my $idx = get_index($lookup, $search);
-
-        cmp_ok($idx, '==', $expected, 'Static Tests');
+        cmp_ok($got, '==', $expected, 'Static Tests');
     }
 }
 
+# anonymous function to get "id" key of an hash
 my $by_id = sub { $_->{id} };
 
 # Test on @data
 my $found_binary = get_index($data_lookup, 10);
-
-## search for 10
 my $found_linear = linear_search(10, $by_id, \@data);
 cmp_ok($found_binary, '==', $found_linear, 'Same result on @data');
 cmp_ok($found_binary, '==', 3,             'Found at index 3');
@@ -102,51 +98,54 @@ cmp_ok(
 ## Pick an existing key - we now that there are 100_000 elements in it
 ## i pick some in the middle
 my $search_for = $big_data[50_000]{id};
-my $bs = get_index($big_data_lookup, $search_for);
-cmp_ok(
-    $bs, '==', linear_search($search_for, $by_id, \@big_data),
-    'Same result on @big_data'
-);
-ok($bs > -1, '$bs found entry');
+my $hs         = get_index($big_data_lookup, $search_for);
+my $ls         = linear_search($search_for, $by_id, \@big_data);
+
+cmp_ok($hs, '==', $ls,    'Hash search same as linear search');
+cmp_ok($hs, '==', 50_000, 'index must be the same as we picked');
 
 ## Search for a key that doesn't exists
-my $bs2 = get_index($big_data_lookup, $max_id+1);
+my $hs2 = get_index($big_data_lookup, $max_id+1);
 my $ls2 = linear_search($max_id+1, $by_id, \@big_data);
 
-cmp_ok($bs2, '==', $ls2, 'Same result when key not exists');
-cmp_ok($bs2, '==', -1,   'key not exists and is bigger than data');
+cmp_ok($hs2, '==', $ls2, 'Same result when key not exists');
+cmp_ok($hs2, '==', -1,   'key does not exists and is bigger than $max_id');
 cmp_ok(
     get_index($big_data_lookup, -100),
     '==',
     linear_search(-100, $by_id, \@big_data),
-    'key does not exists and is small',
+    'key does not exists and is smaller than 0',
 );
 
-
+# Generate some random test by search for random ids
 for my $i ( 1 .. 100 ) {
-    my $id    = int(rand($max_id));
-    my $b_idx = get_index($big_data_lookup, $id);
+    my $id    = int rand $max_id;
+    my $h_idx = get_index($big_data_lookup, $id);
     my $l_idx = linear_search($id, $by_id, \@big_data);
 
-    cmp_ok($b_idx, '==', $l_idx, "random check $i");
+    cmp_ok($h_idx, '==', $l_idx, "random check $i");
 }
 
 done_testing;
 
+
+##- Use CLI switch "-b" to to run Benchmarks
 # Benchmarking
 if ( $opt->benchmark ) {
     cmpthese(-1, {
         'Lookup Table' => sub {
-            my $id = int rand($max_id);
+            my $id = int rand $max_id;
             get_index($big_data_lookup, $id);
         },
         'Linear Search' => sub {
-            my $id = int rand($max_id);
+            my $id = int rand $max_id;
             linear_search($id, $by_id, \@big_data);
         }
     });
 }
 
+
+##- function implementation
 # Linear Search
 sub linear_search($search, $key_by, $data) {
     for my $idx ( 0 .. (scalar @$data) - 1 ) {
