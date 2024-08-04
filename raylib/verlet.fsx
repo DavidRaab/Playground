2f5468bc3db35d7a2cab3d8d505eb7d966eff4aa
David Raab <davidraab83@gmail.com>
2024-08-04 03:14:17 +0200

    Added Mass to system
    Also when things collide a relativeSpeed is calculated what made it look
    better. When objects collide with edges than the velocity is halfed.
    
    Without that and without losing any kind of velocity everything turned
    at some point apeshit.

--- raylib/verlet.fsx
+++ raylib/verlet.fsx
@@ -13,8 +13,8 @@ open System.Numerics
 let screenWidth, screenHeight    = 1200, 800
 let defaultSpeed                 = 100f
 let circleAmount                 = 100
-let gravity                      = vec2 0f 100f
-let circleMinSize, circleMaxSize = 5f, 10f
+let gravity                      = vec2 0f 500f
+let circleMinSize, circleMaxSize = 5f, 15f
 let mutable showVelocity         = false
 
 // Class Alias
@@ -26,33 +26,43 @@ let rng           = System.Random ()
 let nextI min max = rng.Next(min,max)
 let nextF min max = min + (rng.NextSingle() * (max-min))
 
+let vectorMax max (vector:Vector2) =
+    let length = vector.Length ()
+    if length > max
+    then vector * (max / length)
+    else vector
+
 // Data-structures
 type Circle = {
     mutable Position: Vector2
     mutable Velocity: Vector2
-    Acceleration: Vector2
-    Radius:       float32
-    Color:        Color
+    Mass:   float32
+    Radius: float32
+    Color:  Color
 }
 
 module Circle =
-    let randomCircle (speed:float32) = {
-        Position     = vec2 (nextF 0f (float32 screenWidth)) (nextF 0f (float32 screenHeight))
-        Velocity     = (vec2 (nextF -1f 1f) (nextF -1f 1f)) * speed
-        Acceleration = gravity
-        Radius       = nextF circleMinSize circleMaxSize
-        Color        =
-            match nextI 0 5 with
-            | 0 -> Color.DarkBlue
-            | 1 -> Color.Orange
-            | 2 -> Color.Purple
-            | 3 -> Color.SkyBlue
-            | 4 -> Color.DarkGreen
-    }
+    let randomCircle (speed:float32) =
+        let radius = nextF circleMinSize circleMaxSize
+        {
+            Position     = vec2 (nextF 0f (float32 screenWidth)) (nextF 0f (float32 screenHeight))
+            Velocity     = (vec2 (nextF -1f 1f) (nextF -1f 1f)) * speed
+            // Mass depends on object size, but could be different. For visualaization
+            // it makes sense to think a bigger object has more Mass.
+            Mass         = radius
+            Radius       = radius
+            Color        =
+                match nextI 0 5 with
+                | 0 -> Color.DarkBlue
+                | 1 -> Color.Orange
+                | 2 -> Color.Purple
+                | 3 -> Color.SkyBlue
+                | 4 -> Color.DarkGreen
+        }
 
     let update circle (dt:float32) =
         circle.Position <- circle.Position + (circle.Velocity * dt)
-        circle.Velocity <- circle.Velocity + (circle.Acceleration * dt)
+        circle.Velocity <- vectorMax 1000f (circle.Velocity + (gravity * dt))
 
     let draw circle =
         rl.DrawCircle (int circle.Position.X, int circle.Position.Y, circle.Radius, circle.Color)
@@ -75,13 +85,15 @@ module Circle =
                 if distance >= neededDistance then
                     ()
                 else
+                    let relSpeed    = circle.Velocity.Length () - other.Velocity.Length ()
                     let toOther     = toOther / distance // normalize vector
                     let overlap     = (neededDistance - distance)
                     let halfOverlap = (toOther * overlap) / 2f
+                    let mass        = circle.Mass + other.Mass
                     circle.Position <- circle.Position - halfOverlap
-                    circle.Velocity <- (-toOther) * (circle.Velocity.Length())
-                    other.Position  <- other.Position  + halfOverlap
-                    other.Velocity  <- toOther * (other.Velocity.Length())
+                    circle.Velocity <- -toOther * ((2f * other.Mass / mass) * relSpeed)
+                    other.Position  <- other.Position + halfOverlap
+                    other.Velocity  <-  toOther * ((2f * circle.Mass / mass) * relSpeed)
 
     let resolveScreenBoundaryCollision circle =
         let w = float32 screenWidth
@@ -90,19 +102,19 @@ module Circle =
         // Collision with Bottom Axis
         if pos.Y > (h - circle.Radius) then
             circle.Position <- vec2 pos.X (h - circle.Radius)
-            circle.Velocity <- Vector2.Reflect(circle.Velocity, vec2 0f -1f)
+            circle.Velocity <- Vector2.Reflect(circle.Velocity, vec2 0f -1f) * 0.5f
         // Collision with left Axis
         if pos.X < circle.Radius then
             circle.Position <- vec2 circle.Radius pos.Y
-            circle.Velocity <- Vector2.Reflect(circle.Velocity, vec2 1f 0f)
+            circle.Velocity <- Vector2.Reflect(circle.Velocity, vec2 1f 0f) * 0.5f
         // Collision with Right Axis
         if pos.X > (w - circle.Radius) then
             circle.Position <- vec2 (w - circle.Radius) pos.Y
-            circle.Velocity <- Vector2.Reflect(circle.Velocity, vec2 -1f 0f)
+            circle.Velocity <- Vector2.Reflect(circle.Velocity, vec2 -1f 0f) * 0.5f
         // Collision with Up Axis
         if pos.Y < circle.Radius then
             circle.Position <- vec2 pos.X circle.Radius
-            circle.Velocity <- Vector2.Reflect(circle.Velocity, vec2 0f 1f)
+            circle.Velocity <- Vector2.Reflect(circle.Velocity, vec2 0f 1f) * 0.5f
 
 // Circles to draw
 let mutable circles =
