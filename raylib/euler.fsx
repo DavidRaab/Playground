341d062f8e532c677d22ae813596bf8820cbc1a7
David Raab <davidraab83@gmail.com>
2024-08-05 14:06:50 +0200

    Modified Euler

--- raylib/euler.fsx
+++ raylib/euler.fsx
@@ -42,11 +42,11 @@ type Circle = {
 }
 
 module Circle =
-    let randomCircle (speed:float32) =
+    let randomCircle pos =
         let radius = nextF circleMinSize circleMaxSize
         {
-            Position     = vec2 (nextF 0f (float32 screenWidth)) (nextF 0f (float32 screenHeight))
-            Velocity     = (vec2 (nextF -1f 1f) (nextF -1f 1f)) * speed
+            Position     = pos
+            Velocity     = Vector2.Zero
             // Mass depends on object size, but could be different. For visualaization
             // it makes sense to think a bigger object has more Mass.
             Mass         = radius
@@ -131,7 +131,9 @@ module Circle =
 // Circles to draw
 let mutable circles =
     ResizeArray<_>(
-        Seq.init circleAmount (fun i -> Circle.randomCircle defaultSpeed)
+        Seq.init circleAmount (fun i ->
+            Circle.randomCircle (vec2 (randF 0f 1200f) (randF 0f 800f))
+        )
     )
 
 // Game Loop
@@ -146,36 +148,36 @@ while not <| CBool.op_Implicit (rl.WindowShouldClose()) do
     rl.DrawFPS(0,0)
 
     if mouse.Left = Down then
-        let circle = {
-            Circle.randomCircle defaultSpeed with
-                Position = mouse.Position
-        }
-        circles.Add( circle )
+        circles.Add(Circle.randomCircle mouse.Position)
 
-    for circle in circles do
-        // a simulation with 60fps means every movement of every circle is updated
-        // every 1/60. A computer/game/program needs to calculate how much something
-        // moved in this time-frame. Adding substeps of 2 for example means that
-        // on each frame the update runs twice with half the frame-time. So
-        // even when game runs at 60 fps, its simulated as running at 120 fps.
-        // This way the simulation becomes better, collision detection works better
-        // with fast moving objects and so on. But it also costs much performance.
-        //
-        // Instead of running everything at multiple-times of fps someone could
-        // implemented continous collision detection for objects that need it
-        // while everything else just runs at fps or better a fixed update time.
-        let subSteps = 2f
-        let dt = dt / subSteps
-        for i=1 to int subSteps do
+    // a simulation with 60fps means every movement of every circle is updated
+    // every 1/60. A computer/game/program needs to calculate how much something
+    // moved in this time-frame. Adding substeps of 2 for example means that
+    // on each frame the update runs twice with half the frame-time. So
+    // even when game runs at 60 fps, its simulated as running at 120 fps.
+    // This way the simulation becomes better, collision detection works better
+    // with fast moving objects and so on. But it also costs much performance.
+    //
+    // Instead of running everything at multiple-times of fps someone could
+    // implemented continous collision detection for objects that need it
+    // while everything else just runs at fps or better a fixed update time.
+    let subSteps = 2f
+    let dt = dt / subSteps
+    for i=1 to int subSteps do
+        for circle in circles do
             Circle.update circle dt
             Circle.resolveCollision circle circles
             Circle.resolveScreenBoundaryCollision circle
-        Circle.draw   circle
+
+    for circle in circles do
+        Circle.draw circle
 
     rl.DrawText(System.String.Format("Circles: {0}", circles.Count), 1000, 10, 24, Color.Yellow)
 
     if guiButton (rect 325f 10f 150f 30f) "New Circles" then
-        circles <- ResizeArray<_>( Seq.init circleAmount (fun i -> Circle.randomCircle defaultSpeed) )
+        circles <- ResizeArray<_>( Seq.init circleAmount (fun i ->
+            Circle.randomCircle (vec2 (randF 0f 1200f) (randF 0f 800f))
+        ))
     if guiButton (rect 100f 10f 200f 30f) (if showVelocity then "Hide Velocity" else "Show Velocity") then
         showVelocity <- not showVelocity
 
