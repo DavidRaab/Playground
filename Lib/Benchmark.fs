2744eb5dc6e3b0679c92d47d75953ebf6bf0377a
David Raab <davidraab83@gmail.com>
2026-01-16 15:48:45 +0100

    Added something

--- Lib/Benchmark.fs
+++ Lib/Benchmark.fs
@@ -15,3 +15,13 @@ module Whatever =
             result <- f ()
         sw.Stop ()
         result, (sw.Elapsed / float amount)
+
+    let inline bench amount msg ([<InlineIfLambda>] f) =
+        let sw             = System.Diagnostics.Stopwatch.StartNew ()
+        let mutable result = Unchecked.defaultof<_>
+        for i=1 to amount do
+            result <- f ()
+        sw.Stop ()
+        let callsPerSecond = (float amount) / (float sw.ElapsedMilliseconds) * 1000.0
+        printfn "%s %.2f/sec" msg callsPerSecond
+        result, (callsPerSecond)
